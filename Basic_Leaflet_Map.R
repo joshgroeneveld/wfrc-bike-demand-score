029a6b91f7bd651fe06408190ac5a5d14a1533fc
joshgroeneveld <joshua.a.groeneveld@gmail.com>
2022-02-23 07:39:16 -0700

    Initial commit

---
+++ Basic_Leaflet_Map.R
@@ -0,0 +1,39 @@
+library(leaflet)
+library(rgdal)
+library(RColorBrewer)
+
+bike_ped_demand <- readOGR("https://opendata.arcgis.com/datasets/28e4a0950e5a497aaffa8615870167d5_0.geojson")
+
+names(bike_ped_demand)
+summary(bike_ped_demand$BikeDemandScore)
+
+bike_demand_bins <- c(0, 10, 20, 30, 40, 50, 60, 70)
+bike_demand_pal <- colorBin("YlGnBu", domain = bike_ped_demand$BikeDemandScore, bins = bike_demand_bins)
+
+# set pop-up content
+bike_ped_demand$popup <- paste("<strong>", "Zone ID: ", bike_ped_demand$zone_id, "</strong>",
+                               "</br>", "Bike Demand Score: ", bike_ped_demand$BikeDemandScore)
+
+m <- leaflet() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(data = bike_ped_demand,
+              stroke = TRUE,
+              weight = 0.2,
+              color = "#ABABAB",
+              smoothFactor = 0.3,
+              opacity = 1,
+              fillColor = ~bike_demand_pal(BikeDemandScore),
+              fillOpacity = 0.8,
+              popup = ~popup,
+              highlightOptions = highlightOptions(color = "#E2068A",
+                                                  weight = 1.5,
+                                                  bringToFront = TRUE,
+                                                  fillOpacity = 0.5)) %>%
+  addLegend("bottomright",
+            opacity = 1,
+            colors = c("#ffffcc", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#0c2c84"),
+            title = "Bike Demand Score",
+            labels = c("< 10.00", "10.01 - 20.00", "20.01 - 30.00", "30.01 - 40.00", "40.01 - 50.00", "50.01 - 60.00", "60.01 - 70.00"))
+  
+m
+
